28bc060d4bbd3a202a668a2ca3b48c16e9773d2f
Tim Cameron Ryan <id@timryan.org>
2014-09-05 00:18:25 -0700

    Adds tests for string and number values.

--- tests/values.js
+++ tests/values.js
@@ -1,7 +1,11 @@
-console.log('1..4');
+console.log('1..6');
 
 console.log(typeof true == 'boolean' ? 'ok' : 'not ok')
 if (true) console.log('ok'); else console.log('not ok');
 if (false) console.log('not ok'); else console.log('ok');
+
+console.log(typeof 42 == 'number' ? 'ok' : 'not ok')
+
+console.log(typeof 'hi' == 'string' ? 'ok' : 'not ok')
 
 console.log('ok')
