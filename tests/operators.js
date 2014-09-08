dfe6a6c561022a8957cf063597ee8543c0ff725d
Tim Cameron Ryan <id@timryan.org>
2014-09-08 10:39:25 -0700

    Adds modulus operator test.

--- tests/operators.js
+++ tests/operators.js
@@ -1,9 +1,10 @@
-console.log('1..8')
+console.log('1..9')
 
 if (5 + 5 == 10) console.log('ok'); else console.log('not ok');
 if (5 - 5 == 0) console.log('ok'); else console.log('not ok');
 if (5 / 5 == 1) console.log('ok'); else console.log('not ok');
 if (5 * 5 == 25) console.log('ok'); else console.log('not ok');
+if (24 % 5 == 4) console.log('ok'); else console.log('not ok');
 
 console.log(empty('not ok') || 'ok')
 empty('not ok') || console.log('ok');
